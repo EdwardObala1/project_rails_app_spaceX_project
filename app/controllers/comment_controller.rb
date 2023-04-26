b092763e5193b2964ef093ab981ffd01918a48a9
EdwardObala1 <e.obala@alustudent.com>
2023-04-27 01:22:42 +0200

    added commentary

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -4,16 +4,33 @@ class CommentController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(params.require(:comment).permit(:user_id, :comment))
+    # binding.pry
+    @comment = Comment.new(params.require(:comment).permit(:comment))
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to request.referrer, notice: "Changes saved successfully"
+    else
+      render html: @comment.errors, status: :unprocessable_entity
+    end
   end
 
   def update
     @comment = Comment.find_by(id: params[:id])
     @comment.update(comment: params[:comment][:comment])
+    if @comment.save
+      redirect_to request.referrer, notice: "Changes saved successfully"
+    else
+      render html: @comment.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @comment = Comment.find_by(id: params[:id])
     @comment.destroy
+    if @comment.save
+      redirect_to request.referrer, notice: "Changes saved successfully"
+    else
+      render html: @comment.errors, status: :unprocessable_entity
+    end
   end
 end
